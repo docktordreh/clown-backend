19de61871f259a482bd2553c2a995982d2f2cc1b
valentin lechner <valentin_lechner@dismail.de>
2023-09-28 17:07:41 +0200

    Implement POST

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -2,7 +2,27 @@ const db = require("../models");
 const Model = db.model;
 const Op = db.sequelize.Op;
 exports.create = (req, res) => {
-
+  let fields = ["coffee", "grinder", "grindsetting", "amtbeans", "brewingtime", "weightres", "taste", "time"];
+  let hasContent = false;
+  for ( let [key, value] of Object.entries(req.body)) {
+    hasContent = value ? true && fields.includes(key) : hasContent;
+  }
+  if (!hasContent) {
+    res.status(500).send({message: `Invalid arguments passed. Valid: ${fields} `})
+  }
+  const model = {
+    createdAt: req.body.time,
+    coffee: req.body.coffee,
+    grinder: req.body.grinder,
+    grindsetting: req.body.grindsetting,
+    amtbeans: req.body.amtbeans,
+    brewingtime: req.body.brewingtime,
+    weightres: req.body.weightres,
+    taste: req.body.taste
+  };
+  Model.create(model).then(data => {res.send(data);}).catch(err => {
+    res.status(500).send({message: err.message || "Error while creating the Model."});
+  });
 };
 exports.findAll = (req, res) => {
 
